0cd19266e2d85c750be241aec24ae998f0322615
yizjiang <leo.jyz@gmail.com>
2013-10-26 11:19:16 -0700

    init commit

---
+++ app/models/todo.rb
@@ -0,0 +1,3 @@
+class Todo < ActiveRecord::Base
+  attr_accessible :complete, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = 'd27924030841832c303f617e3004671bd7140f8fa1c43f2a0e2754a1e107d5908f98e004fc88ea3c9dac784a0c8d86e7a325cd996ebf4298aef12aad11458512'
